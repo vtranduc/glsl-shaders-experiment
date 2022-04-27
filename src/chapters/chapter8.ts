387206e376a06a8594949274c2423b89a66adc95
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2022-04-27 17:14:44 -0400

    added more features and functionalities for rectangle demo

--- src/chapters/chapter8.ts
+++ src/chapters/chapter8.ts
@@ -1,8 +1,19 @@
 import * as THREE from "three";
 
+type RectangleIdentifier = 0 | 1 | 2;
+
 export class Chapter8 {
-  private geo = new THREE.PlaneGeometry(2, 2);
+  private rect: RectangleIdentifier = 0;
+  private geo = new THREE.PlaneGeometry(20, 20);
   private mat = new THREE.ShaderMaterial({
+    uniforms: {
+      u_size1: { value: { x: 0.5, y: 0.8 } },
+      u_center1: { value: { x: 0, y: 0.5 } },
+      u_size2: { value: { x: 1, y: 0.8 } },
+      u_center2: { value: { x: -0.5, y: 0 } },
+      u_size3: { value: { x: 1, y: 0.8 } },
+      u_center3: { value: { x: 3, y: 0 } },
+    },
     vertexShader: `
         varying vec3 v_position;
         void main() {
@@ -13,21 +24,91 @@ export class Chapter8 {
     `,
     fragmentShader: `
         varying vec3 v_position;
+        uniform vec2 u_size1;
+        uniform vec2 u_center1;
+        uniform vec2 u_size2;
+        uniform vec2 u_center2;
+        uniform vec2 u_size3;
+        uniform vec2 u_center3;
 
-        float isRect(vec2 position, vec2 size) {
+        float isRect(vec2 position, vec2 size, vec2 center) {
             vec2 halfSize = size / 2.0;
-            vec2 isInside = step(-halfSize, position) - step(halfSize, position);
+            vec2 isInside = step(center - halfSize, position)
+                - step(center + halfSize, position);
             return isInside.x * isInside.y;
         }
 
         void main() {
-            vec2 size = vec2(0.5, 0.5);
-            float r = isRect(v_position.xy, size);
-            gl_FragColor = vec4(r, 0.0, 1.0, 1.0);
+            float r = isRect(v_position.xy, u_size1, u_center1);
+            float g = isRect(v_position.xy, u_size2, u_center2);
+            float b = isRect(v_position.xy, u_size3, u_center3);
+            gl_FragColor = vec4(r, g, b, 1.0);
         }
     `,
   });
   private mesh = new THREE.Mesh(this.geo, this.mat);
+
+  public get x() {
+    return this.uniformsPosition.x;
+  }
+
+  public set x(val: number) {
+    this.uniformsPosition.x = val;
+  }
+
+  public get y() {
+    return this.uniformsPosition.y;
+  }
+
+  public set y(val: number) {
+    this.uniformsPosition.y = val;
+  }
+
+  public get xSize() {
+    return this.uniformsSize.x;
+  }
+
+  public set xSize(val: number) {
+    if (val < 0) return;
+    this.uniformsSize.x = val;
+  }
+
+  public get ySize() {
+    return this.uniformsSize.y;
+  }
+
+  public set ySize(val: number) {
+    if (val < 0) return;
+    this.uniformsSize.y = val;
+  }
+
+  private get uniformsPosition() {
+    switch (this.rect) {
+      case 1:
+        return this.mat.uniforms.u_center2.value;
+      case 2:
+        return this.mat.uniforms.u_center3.value;
+      case 0:
+      default:
+        return this.mat.uniforms.u_center1.value;
+    }
+  }
+
+  private get uniformsSize() {
+    switch (this.rect) {
+      case 1:
+        return this.mat.uniforms.u_size2.value;
+      case 2:
+        return this.mat.uniforms.u_size3.value;
+      case 0:
+      default:
+        return this.mat.uniforms.u_size1.value;
+    }
+  }
+
+  public switch() {
+    this.rect = (++this.rect % 3) as RectangleIdentifier;
+  }
 
   public get scene() {
     return this.mesh;
--- src/saga/chapters/chapter8Saga.ts
+++ src/saga/chapters/chapter8Saga.ts
@@ -1,7 +1,47 @@
+import { PayloadAction } from "@reduxjs/toolkit";
 import { CanvasManager } from "../../utils";
 import { Chapter8 } from "../../chapters";
+import { Press, Arrow, Special, Key } from "../../types";
+import { takeEvery } from "redux-saga/effects";
+import { press } from "../../reducers";
 
-export function chapter8Saga(controller: CanvasManager) {
+export function* chapter8Saga(controller: CanvasManager) {
   const chapter8 = new Chapter8();
   controller.add(chapter8.scene);
+  yield takeEvery(press.type, chapter8CommandsSaga(chapter8));
+}
+
+function chapter8CommandsSaga(chapter8: Chapter8) {
+  return function ({ payload }: PayloadAction<Press>) {
+    switch (payload) {
+      case Arrow.Up:
+        chapter8.ySize += 0.1;
+        break;
+      case Arrow.Down:
+        chapter8.ySize -= 0.1;
+        break;
+      case Arrow.Right:
+        chapter8.xSize += 0.1;
+        break;
+      case Arrow.Left:
+        chapter8.xSize -= 0.1;
+        break;
+      case Special.Space:
+        chapter8.switch();
+        break;
+      case Key.D:
+        chapter8.x += 0.1;
+        break;
+      case Key.A:
+        chapter8.x -= 0.1;
+        break;
+      case Key.W:
+        chapter8.y += 0.1;
+        break;
+      case Key.S:
+        chapter8.y -= 0.1;
+        break;
+      default:
+    }
+  };
 }
