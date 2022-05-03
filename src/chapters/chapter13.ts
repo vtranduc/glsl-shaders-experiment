c262c71133fa4ec375dce8d8ab3b21161fda67ad
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2022-05-03 16:15:47 -0400

    added lightning to shaders

--- src/chapters/chapter13.ts
+++ src/chapters/chapter13.ts
@@ -5,20 +5,19 @@ export class Chapter13 {
   private geo = new THREE.BoxGeometry(30, 30, 30, 10, 10, 10);
   private mat = new THREE.ShaderMaterial({
     wireframe: true,
+    lights: true,
     uniforms: {
-      u_time: { value: this.clock.getElapsedTime() },
-      u_radius: { value: 30 },
+      ...THREE.UniformsUtils.merge([
+        THREE.UniformsLib["common"],
+        THREE.UniformsLib["lights"],
+      ]),
+      ...{
+        u_time: { value: this.clock.getElapsedTime() },
+        u_radius: { value: 30 },
+        u_color: { value: { r: 1.0, g: 0.0, b: 0.0 } },
+      },
     },
-    vertexShader: `
-        uniform float u_time;
-        uniform float u_radius;
-        void main() {
-            float delta = (sin(u_time * 3.0) + 1.0) / 2.0;
-            vec3 pos = mix(position, normalize(position) * u_radius, delta);
-            gl_Position = projectionMatrix * modelViewMatrix
-                * vec4(pos, 1.0);
-        }
-    `,
+    ...this.getShaders(true),
   });
 
   private mesh = new THREE.Mesh(this.geo, this.mat);
@@ -27,8 +26,107 @@ export class Chapter13 {
     this.updateOnAnimationFrame = this.updateOnAnimationFrame.bind(this);
   }
 
+  public toggleWireframe() {
+    this.mat.wireframe = !this.mat.wireframe;
+  }
+
   public updateOnAnimationFrame() {
     this.mat.uniforms.u_time.value = this.clock.getElapsedTime();
+  }
+
+  public get lights() {
+    return this.mat.lights;
+  }
+
+  public set lights(lights: boolean) {
+    if (this.lights === lights) return;
+    const { vertexShader, fragmentShader } = this.getShaders(lights);
+    this.mat.vertexShader = vertexShader;
+    this.mat.fragmentShader = fragmentShader;
+    this.mat.lights = lights;
+    this.mat.needsUpdate = true;
+  }
+
+  private getShaders(lights: boolean) {
+    return {
+      vertexShader: `
+            ${
+              lights
+                ? `
+                    #include <common>
+                    #include <lights_pars_begin>
+                    varying vec3 vLightIntensity;
+                `
+                : ""
+            }
+
+            uniform float u_time;
+            uniform float u_radius;
+
+            void main() {
+                float delta = (sin(u_time * 3.0) + 1.0) / 2.0;
+                vec3 normalSphere = normalize(position);
+
+                ${
+                  lights
+                    ? `
+                        vec3 vLightFront;
+                        vec3 vIndirectFront;
+                        vec3 objectNormal = mix(normal, normalSphere, delta);
+                        #include <defaultnormal_vertex>
+                        #include <begin_vertex>
+                        #include <project_vertex>
+                        #include <lights_lambert_vertex>
+                        vLightIntensity = vLightFront + ambientLightColor;
+                    `
+                    : ""
+                }
+                
+                vec3 pos = mix(position, normalSphere * u_radius, delta);
+                gl_Position = projectionMatrix * modelViewMatrix
+                    * vec4(pos, 1.0);
+            }
+        `,
+      fragmentShader: `
+            ${lights ? "varying vec3 vLightIntensity;" : ""}
+            uniform vec3 u_color;
+            void main() {
+                gl_FragColor = vec4(${
+                  lights ? "vLightIntensity * 0.5 * " : ""
+                }u_color, 1.0);
+            }
+        `,
+    };
+  }
+
+  public get r() {
+    return this.mat.uniforms.u_color.value.r;
+  }
+
+  public set r(val: number) {
+    if (val > 1) this.mat.uniforms.u_color.value.r = 1;
+    else if (val < 0) this.mat.uniforms.u_color.value.r = 0;
+    else this.mat.uniforms.u_color.value.r = val;
+  }
+
+  public get g() {
+    return this.mat.uniforms.u_color.value.g;
+  }
+
+  public set g(val: number) {
+    if (val > 1) this.mat.uniforms.u_color.value.g = 1;
+    else if (val < 0) this.mat.uniforms.u_color.value.g = 0;
+    else this.mat.uniforms.u_color.value.g = val;
+  }
+
+  public get b() {
+    return this.mat.uniforms.u_color.value.b;
+  }
+
+  public set b(val: number) {
+    if (val > 1) this.mat.uniforms.u_color.value.b = 1;
+    else if (val < 0) this.mat.uniforms.u_color.value.b = 0;
+    else this.mat.uniforms.u_color.value.b = val;
   }
 
   public get scene() {
--- src/saga/chapters/chapter13Saga.ts
+++ src/saga/chapters/chapter13Saga.ts
@@ -1,9 +1,46 @@
+import { takeEvery } from "redux-saga/effects";
+import { PayloadAction } from "@reduxjs/toolkit";
 import { Chapter13 } from "../../chapters";
 import { CanvasManager } from "../../utils";
+import { Press, Special } from "../../types";
+import { press } from "../../reducers";
 
-export function chapter13Saga(controller: CanvasManager) {
+export function* chapter13Saga(controller: CanvasManager) {
   const chapter13 = new Chapter13();
   controller.add(chapter13.scene);
   controller.setCameraPosition(50, 50, 50);
   controller.requestAnimation(chapter13.updateOnAnimationFrame);
+  yield takeEvery(press.type, chapter13CommandsSaga(chapter13));
+}
+
+function chapter13CommandsSaga(chapter13: Chapter13) {
+  return function ({ payload }: PayloadAction<Press>) {
+    switch (payload) {
+      case Special.BracketRight:
+        chapter13.r += 0.1;
+        break;
+      case Special.BracketLeft:
+        chapter13.r -= 0.1;
+        break;
+      case Special.Quote:
+        chapter13.g += 0.1;
+        break;
+      case Special.Semicolon:
+        chapter13.g -= 0.1;
+        break;
+      case Special.Slash:
+        chapter13.b += 0.1;
+        break;
+      case Special.Period:
+        chapter13.b -= 0.1;
+        break;
+      case Special.Space:
+        chapter13.toggleWireframe();
+        break;
+      case Special.Enter:
+        chapter13.lights = !chapter13.lights;
+        break;
+      default:
+    }
+  };
 }
